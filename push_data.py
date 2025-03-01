e09ad0ca054e76eeda0f9300ebbe4cac6e6e919e
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-01 12:36:50 +0530

    Update push_data.py
    The code connects and pushes data to mongodb atlas database.

--- push_data.py
+++ push_data.py
@@ -10,3 +10,48 @@ if MONGO_DB_URL:
     print(MONGO_DB_URL)
 else:
     print("MONGO_DB_URL is not set.")
+
+import certifi
+ca = certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging import logger
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json_convertor(self, file_path):
+        try:
+            data = pd.read_csv(file_path)
+            data.reset_index(drop=True, inplace=True)
+            # return data.to_dict(orient='records'), same as the below code but much simpler
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self, records, database, collection):
+
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+        
+            return (len(self.records), "records inserted successfully")
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+            
