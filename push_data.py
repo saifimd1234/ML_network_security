64f19106f067186d8e07f4154f80c2260647addf
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-01 14:10:37 +0530

    ETL pipeline completed.
    After inserting this part and running it in terminal we get: (11055, 'records inserted successfully'). Check MongoDB atlas for further clarity.

--- push_data.py
+++ push_data.py
@@ -55,3 +55,12 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
             
+if __name__=='__main__':
+    FILE_PATH = "Network_Data\phisingData.csv"
+    DATABASE = "SaifiAI"
+    Collection="NetworkData"
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_convertor(FILE_PATH)
+    print(records)
+    no_of_records = networkobj.insert_data_mongodb(records, DATABASE, Collection)
+    print(no_of_records)
