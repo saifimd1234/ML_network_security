6884bf324d4bde614c0ff2c77dea76b9554050ad
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-03 19:53:38 +0530

    Implement MLflow in model_trainer.py
    While activating the same env, in which everything has been created and after you run main.py. A folder named mlruns in created with all the details. Use: mlflow ui, to get the link of the MLflow logs.

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -21,6 +21,8 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+import mlflow
+
 
 class ModelTrainer:
     """
@@ -37,7 +39,21 @@ class ModelTrainer:
         
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-        
+
+    def track_mlflow(self, best_model, classificationmetric):
+
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+
+
     def train_model(self, X_train, y_train, X_test, y_test):
         models = {
             "Random Forest": RandomForestClassifier(verbose=1),
@@ -90,8 +106,14 @@ class ModelTrainer:
 
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
+        ## Track the train experiements with mlflow
+        self.track_mlflow(best_model, classification_train_metric)
+
         y_test_pred = best_model.predict(X_test)
         classification_test_metric = get_classification_score(y_true=y_test, y_pred=y_test_pred)
+
+        ## Track the test experiements with mlflow
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
