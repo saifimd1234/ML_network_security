cea205f054af79fe89c567f8db04bc9e3103a736
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-04 02:58:42 +0530

    Update app.py for batch predictions.

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+# This below code is to be written now to use the templates for table.html.
 from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="./templates")
 
@@ -82,6 +82,40 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
+@app.post("/predict")
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    """
+    Endpoint to make predictions using the trained network security model.
+    
+    This endpoint accepts a CSV file upload, processes it through the trained model, and returns predictions as an HTML table.
+    
+    Args:
+        request (Request): The FastAPI request object.
+        file (UploadFile): The uploaded CSV file containing features for prediction.
+        
+    Returns:
+        TemplateResponse: HTML template with the prediction results displayed as a table.
+        
+    Raises:
+        NetworkSecurityException: If any error occurs during the prediction process.
+    """
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        print(df['predicted_column'])
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+        
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
 # Entry point of this app
 if __name__ == "__main__":
     app_run(app, host="localhost", port=8000)
