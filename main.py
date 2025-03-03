48352e60af24c9c663364b1b896e6b77238cc49b
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-03 11:32:48 +0530

    Testing if data_validation.py is working fine or not.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -14,8 +16,15 @@ if __name__ == '__main__':
         logging.info("Data Ingestion object created successfully.")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion process completed successfully.")
-
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data validation.")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        
+        logging.info("Data Validation process completed successfully.")
+        print(dataingestionartifact)
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
