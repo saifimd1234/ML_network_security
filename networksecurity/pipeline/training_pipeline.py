f01aaccb4b80c2df9c3082902cad595201f11e4b
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-06 14:31:20 +0530

    Implement S3 bucket in training_pipeline.
    Create related function to sync_artifact_dir_to_s3 and sync_saved_model_dir_to_s3 and call them in run_pipeline.

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -24,6 +24,10 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
+from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.cloud.s3_syncer import S3Sync
+from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
+
 import sys
 
 
@@ -45,6 +49,7 @@ class TrainingPipeline:
         Creates a TrainingPipelineConfig instance that contains timing and path information for the current training run, and initializes the S3Sync utility for cloud storage.
         """
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
         
 
     def start_data_ingestion(self):
@@ -162,6 +167,41 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)    
     
+    def sync_artifact_dir_to_s3(self):
+        """
+        Synchronize local artifacts directory to S3 bucket.
+        
+        This method uploads all artifacts generated during the pipeline execution
+        (including intermediate datasets, validation reports, and transformation objects) to a timestamped location in the configured S3 bucket for tracking and reproducibility.
+        
+        Raises:
+            NetworkSecurityException: If synchronization to S3 fails
+        """
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir, aws_bucket_url=aws_bucket_url)
+        
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def sync_saved_model_dir_to_s3(self):
+        """
+        Synchronize local model directory to S3 bucket.
+        
+        This method uploads the final trained model to a timestamped location
+        in the configured S3 bucket for later use in deployment or inference.
+        This ensures model versioning and availability in the cloud.
+        
+        Raises:
+            NetworkSecurityException: If synchronization to S3 fails
+        """
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.model_dir, aws_bucket_url=aws_bucket_url)
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
     def run_pipeline(self):
         """
         Execute the complete training pipeline from end to end.
@@ -185,6 +225,9 @@ class TrainingPipeline:
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
             
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
+
             return model_trainer_artifact
         
         except Exception as e:
